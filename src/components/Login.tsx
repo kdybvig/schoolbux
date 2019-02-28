0cb072dc0ab7a57c2096c5d690e48da0fd8d597a
kdybvig <kdybvig@Keiths-MacBook-Pro.local>
2019-02-27 20:00:08 -0600

    added forms for login and register

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,29 @@
 import React, { FunctionComponent, useState } from 'react';
+import Form from "react-bootstrap/Form";
+import './UserForms.css';
+import Button from "react-bootstrap/Button";
 
 const Login:FunctionComponent = () => {
     const user = useState('');
+    const password = useState('');
 
     return (
-      <div>Login Page</div>
+      <div className="col-sm-6 offset-sm-3">
+        <h1 className="user-form-title">Sign In</h1> 
+        <Form>
+          <Form.Group controlId="loginUser">
+            <Form.Label className="user-form-label">Email or username</Form.Label>
+            <Form.Control type="email" placeholder="Enter email or username" />
+          </Form.Group>
+          <Form.Group controlId="loginPassword">
+            <Form.Label className="user-form-label">Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="success" type="submit">
+            Sign In
+          </Button>
+        </Form>
+      </div>
     )
 }
 
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,11 +1,44 @@
 import React, { FunctionComponent, useState } from 'react';
+import Form from "react-bootstrap/Form";
+import './UserForms.css';
+import Button from "react-bootstrap/Button";
 
 const Register:FunctionComponent = () => {
-    const user = useState('');
 
-    return (
-      <div>Register Page</div>
-    )
+  const user = useState('');
+  
+  return (
+    <div className="col-sm-6 offset-sm-3">
+      <h1 className="user-form-title">Create account</h1> 
+      <Form>
+        <Form.Group controlId="registerEmail">
+          <Form.Label className="user-form-label">Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="registerUsername">
+          <Form.Label className="user-form-label">Username</Form.Label>
+          <Form.Control type="username" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="registerPassword">
+          <Form.Label className="user-form-label">Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="registerConfirm">
+          <Form.Label className="user-form-label">Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="registerChecbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="success" type="submit">
+          Register
+        </Button>
+      </Form>
+    </div>
+  )
 }
 
 export default Register;
