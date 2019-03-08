105d46eeb9f71552ca92a01f9982585bcb4ccb8c
kdybvig <kdybvig@Keiths-MacBook-Pro.local>
2019-03-07 21:21:27 -0600

    implemented form control

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,24 +4,20 @@ import './UserForms.css';
 import Button from "react-bootstrap/Button";
 
 const Login:FunctionComponent = () => {
-    const user = useState('');
-    const password = useState('');
-
-    const handleChange = () => {
-      
-    }
-
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    console.log('user: ', user, 'password: ', password)
     return (
       <div className="col-sm-6 offset-sm-3">
         <h1 className="user-form-title">Sign In</h1> 
         <Form>
           <Form.Group controlId="loginUser">
             <Form.Label className="user-form-label">Email or username</Form.Label>
-            <Form.Control type="email" placeholder="Enter email or username" />
+            <Form.Control type="email" placeholder="Enter email or username" value={user} onChange={(e: any) => setUser(e.target.value)} />
           </Form.Group>
           <Form.Group controlId="loginPassword">
             <Form.Label className="user-form-label">Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" onChange={(e: any) => setPassword(e.target.value)}/>
           </Form.Group>
           <Button variant="success" type="submit">
             Sign In
