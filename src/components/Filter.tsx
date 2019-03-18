46a39c15f9c95419e7f0c707948fa9afbdd85b28
kdybvig <kdybvig@Keiths-MacBook-Pro.local>
2019-03-18 09:20:36 -0500

    moved inline styles for Filter to stylesheet

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,23 +1,12 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
+import './Filter.css'
+
 
 const Filter:FunctionComponent = () => {
 
 
     return (
-        <div className="dark-filter" style={
-            {
-                position:'fixed',
-                padding:'0',
-                margin:'0',
-                
-                top:0,
-                left: 0,
-                zIndex: 5000,
-                width: '100%',
-                height: '100%',
-                background:'rgba(0,0,0,0.3)'
-            }
-        }>
+        <div className="dark-filter" >
         
         </div>
     )
