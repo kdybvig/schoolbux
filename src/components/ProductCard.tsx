e06c65891bcf638ccc6ec2bb475aafb316eba671
kdybvig <kdybvig@Keiths-MacBook-Pro.local>
2019-03-22 10:36:44 -0500

    removed unnecessary inline styles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -24,7 +24,7 @@ const ProductCard: FunctionComponent<ProductCardProps> = (props) => {
     const {imgUrl, title, price, description, inStock} = props;
 
     return (
-        <Card className='product-card' style={{}/*{ width: '200px', backgroundColor: '#EAE3D7', border:'1px solid #CCC', padding: '15px', paddingBottom: '45px'}*/}>
+        <Card className='product-card'>
             <Card.Img style={{border: '1px solid #AAA', width: 170, height: 170 }}variant="top" src={imgUrl} />
             <hr />
             <Card.Title style = {{marginBottom: 5}}>{title}</Card.Title>
